3d93b61422a3b1cee91e80c8e5d9bf1bb872f089
Ubuntu <ubuntu@ip-172-31-38-125.ec2.internal>
2020-10-31 00:30:09 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+mysqli_connect("db", "root", "mysql.rootp4ss") or die(mysqli_error()); 
+
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "  <head>";
+echo "    <meta charset=\"UTF-8\"/>";
+echo "    <title>Hello World!</title>";
+echo "  </head>";
+echo "  <body>";
+echo "    <h1>Hello World!<h1>";
+echo "  </body>";
+echo "</html>";
+
+?>
