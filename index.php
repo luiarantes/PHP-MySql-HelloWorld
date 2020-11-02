5bb6a76137f1116c66067ac8c2e0920595ff2c3a
Luiz Fernando Arantes <ti@luiarantes.com>
2020-11-02 03:30:54 +0000

    DB connections with include Fix

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php 
 
-<<<<<<< HEAD
-mysqli_connect("mysql-inst-01.c973bu7odyi3.us-east-1.rds.amazonaws.com", "root", "mysql.rootp4ss") or die(mysqli_error()); 
-=======
 include 'db.php';
->>>>>>> 4d229a7bd4efbea84801c3e0a6b2a28a21956016
 
 echo "<!DOCTYPE html>";
 echo "<html>";
